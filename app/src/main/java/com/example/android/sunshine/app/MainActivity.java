ef2f4fa165fa60d317a1dff344190ee3d9972415
Runn3rsHigh <kschmidty17@gmail.com>
2016-10-13 00:12:53 -0700

    Renamed PlaceholderFragment to ForecastFragment.  Moved into its own class.

--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.android.sunshine.app;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -11,6 +12,12 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 
 
@@ -47,33 +54,5 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        private ArrayAdapter<String> mAdapter;
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-
-            ArrayList<String> fakeData = new ArrayList<String>();
-            fakeData.add("Today-Sunny-78/60");
-            fakeData.add("Monday-Overcast-74/55");
-            fakeData.add("Tuesday-Cloudy-76/63");
-
-            mAdapter = new ArrayAdapter<String>(getActivity(),R.layout.list_item_forecast,R.id.list_item_forecast_textview,fakeData);
-
-            ListView mListView = (ListView)rootView.findViewById(R.id.listview_forecast);
-            mListView.setAdapter(mAdapter);
-
-            return rootView;
-        }
     }
 }
